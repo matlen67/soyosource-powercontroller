06cb0d25207265bbf3269c974422035e6209fbea
matten <matthias.lenkeit@gmail.com>
2024-10-26 10:55:18 +0200

    add Darkmode

--- src/html.h
+++ src/html.h
@@ -5,6 +5,78 @@ const char index_html[] PROGMEM = R"rawliteral(
   <title>SoyoSource-PowerController</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <style>
+
+    :root{
+    --base-color: #fefefe;
+    --base-variant: #fafafa;
+    --text-color: #0f0f0f;
+    --t1-color:   #090909;
+    --card-shadow: #06060680;
+    --topnav-color: #263c5e;
+    --button-color: #263c5e;
+    --input-bgcolor: #fefefe;
+    --secondary-text: #232738;
+    --primary-color: #3a435d;
+    --accent-color: #0099ff;
+    }
+
+    .darkmode{
+      --base-color:   #0c0e0f;
+      --base-variant: #0a0c0d;
+      --text-color:   #fafafa;
+      --t1-color:     #6e6e6e;
+      --card-shadow: #59758880;
+      --topnav-color: #202429;
+      --button-color: #243641;
+      --input-bgcolor: #1f1f22;
+      --secondary-text:#a4a5b8;
+      --primary-color: #3a435d;
+      --accent-color:  #00b9d1;
+    }
+
+    section{
+      background-color: var(--base-variant);
+    }
+
+    button{
+      border: none;
+      padding: .8em 2em;
+      background-color: var(--primary-color);
+      color: #ffffff;
+      border-radius: 4px;
+      font: inherit;
+      text-transform: uppercase;
+      font-weight: 500;
+      cursor: pointer;
+    }
+
+    #theme-switch{
+      height: 40px;
+      width: 40px;
+      padding: 0;
+      border-radius: 50%;
+      background-color: var(--base-variant);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: fixed;
+      top: 10px;
+      right: 10px;
+    }
+
+    #theme-switch svg{
+      fill: var(--primary-color);
+    }
+    #theme-switch svg:last-child{
+      display: none;
+    }
+    .darkmode #theme-switch svg:first-child{
+      display: none;
+    }
+    .darkmode #theme-switch svg:last-child{
+      display: block;
+    }
+
     html {font-family: Arial; display: inline-block; text-align: center;}
     table { width: 90%; table-layout: fixed; }
     table.font1{ font-size: clamp(.9rem, 0.9vw, 1.2rem); }
@@ -12,19 +84,19 @@ const char index_html[] PROGMEM = R"rawliteral(
     td { text-align: left; }
     th { text-align: center; }
     p {  font-size: 1.2rem;}
-    body {  margin: 0;}
-    .title1 { font-size: 1.2rem; font-weight: bold; color: #034078; }
+    
+    body {  
+      margin: 0;
+      background-color: var(--base-color);
+      color: var(--text-color);
+    }
+    
+    .title1 { font-size: 1.2rem; font-weight: bold; color: --text-color; }
     .alnright { text-align: right; }
-    .topnav { overflow: hidden; background-color: #263c5e; color: white; font-size:1.5rem; margin:0px; padding:0px }
+    .topnav { overflow: hidden; background-color:var(--topnav-color); color: white; font-size:1.5rem; margin:0px; padding:0px }
     .content { padding: 10px; }
-    .card { background-color: white; padding: 5px; box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5); }
+    .card { background-color: var(--base-variant) ; padding: 5px; box-shadow: 2px 2px 12px 1px var(--card-shadow); }
     .card-grid { max-width: 700px; margin: 0 auto; display: grid; grid-gap: 2rem; grid-template-columns: repeat(auto-fit, minmax(20em, 1fr)); }
-    .reading { font-size: 2.0rem; }
-    .headline { font-size: 2.0rem; font-weight: bold; }
-    .card.info { color: #034078; }
-    .card.info-dark { color: #151515; }
-    .card-title { font-size: 1.2rem; font-weight: bold; color: #034078; }
-    .card-title-dark { font-size: 1.2rem; font-weight: bold; color: #151515; }
     .btn:active { transform: scale(0.80); box-shadow: 3px 2px 22px 1px rgba(0, 0, 0, 0.24); }
     .btn {
             text-decoration: none;
@@ -34,8 +106,8 @@ const char index_html[] PROGMEM = R"rawliteral(
             margin: 15px 5px 15px 5px;
             text-align: center;
             font-size: 16px;
-            background-color:#034078;
-            color: #fff;
+            background-color: var(--button-color);
+            color: #fefefe;
             border-radius: 5px;
             box-shadow: 7px 6px 28px 1px rgba(0, 0, 0, 0.24);
             cursor: pointer;
@@ -60,7 +132,7 @@ const char index_html[] PROGMEM = R"rawliteral(
     }
 
     input:invalid {
-        background-color: rgb(255, 179, 190);
+        background-color: rgb(255, 197, 197);
     }
 
     input[type=text], select {
@@ -70,6 +142,8 @@ const char index_html[] PROGMEM = R"rawliteral(
         border: 1px solid #ccc;
         border-radius: 4px;
         box-sizing: border-box;
+        color: var(--text-color);
+        background-color: var(--input-bgcolor);
     }
 
     input[type=number], select {
@@ -79,6 +153,8 @@ const char index_html[] PROGMEM = R"rawliteral(
         border: 1px solid #ccc;
         border-radius: 4px;
         box-sizing: border-box;
+        color: var(--text-color);
+        background-color: var(--input-bgcolor);
     }
 
     input[type=time], select {
@@ -88,19 +164,21 @@ const char index_html[] PROGMEM = R"rawliteral(
         border: 1px solid #ccc;
         border-radius: 4px;
         box-sizing: border-box;
+        color: var(--text-color);
+        background-color: var(--input-bgcolor);
     }     
      
     hr {
         border: 0;
         height: 1px;
-        background-image: linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0));
+        background-color: var(--t1-color);;
     }
    
     summary {
         position: relative;
         padding: 12px;
         display: flex;
-        color: #102752;
+        color: var(--text-color);
         transform: scaleX(-1);
     }
 
@@ -186,7 +264,7 @@ const char index_html[] PROGMEM = R"rawliteral(
    
     .version {
       top: -40%;
-      right: -165px;
+      right: -115px;
       font-size: 40%;
       margin-top: -25px;
       margin-bottom: 10px;
@@ -194,13 +272,19 @@ const char index_html[] PROGMEM = R"rawliteral(
     }
 
   </style>
-
 </head>
+
 <body onload="uhrzeit()">
 
   <div class="topnav">
-    <h3>SoyoSource-PowerController</h3>
-    <div class="version">v 1.241013</div>
+    <h5>SoyoSource-PowerController</h5>
+    <div class="version">v 1.241026</div>
+    <div>
+      <button id="theme-switch">
+        <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px"><path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Z"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px"><path d="M480-280q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Z"/></svg>
+      </button>
+    </div>
   </div>
 
   <div class="content">
@@ -691,9 +775,30 @@ const char index_html[] PROGMEM = R"rawliteral(
   var options = { year: 'numeric', month: '2-digit', day: '2-digit' };
   document.getElementById("datum").textContent = mydate.toLocaleString('de-DE', options);
 
-  
+
+  let darkmode = localStorage.getItem('darkmode')
+  const themeSwitch = document.getElementById('theme-switch')
+
+  const enableDarkmode = () => {
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode', 'active')
+  }
+
+  const disableDarkmode = () => {
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkmode', null)
+  }
+
+  if(darkmode === "active") enableDarkmode()
+
+  themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem('darkmode')
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+  })
+
 </script>
 
 </body>
 </html>
+
 )rawliteral";
